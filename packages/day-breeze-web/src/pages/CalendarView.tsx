6a008c0f9c9c86c34a9c8d341eefe6ceac1bd019
Yun Cheolhun <oliverne@gmail.com>
2020-02-07 16:24:55 +0900

    Fix module path

--- packages/day-breeze-web/src/pages/CalendarView.tsx
+++ packages/day-breeze-web/src/pages/CalendarView.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
-import { Calendars } from '/src/components/Calendars'
+import { Calendars } from '../components/Calendars'
 
 export const CalendarView: React.FC<RouteComponentProps> = () => {
   return <Calendars />
