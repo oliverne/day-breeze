92f0494ad72641346426e7ea10c0a50609ff93d4
Yun Cheolhun <oliverne@gmail.com>
2020-02-16 01:26:39 +0900

    Change button text

--- packages/day-breeze-web/src/pages/Home.tsx
+++ packages/day-breeze-web/src/pages/Home.tsx
@@ -3,14 +3,14 @@ import { Link, RouteComponentProps } from '@reach/router'
 
 export const Home: React.FC<RouteComponentProps> = () => {
   return (
-    <div className="home">
+    <div className="Home">
       <div className="hero">
         <div className="hero-body">
           <h1>Day Breeze</h1>
           <p>Daily Memo</p>
           <Link to="/calendar">
             <button type="button" className="btn btn-lg login">
-              FREE LOGIN
+              DEMO LOGIN
             </button>
           </Link>
         </div>
