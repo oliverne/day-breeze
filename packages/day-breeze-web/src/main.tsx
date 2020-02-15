d7c048a57daaa4fe4fbc8a9e9f1321c13cd17259
Yun Cheolhun <oliverne@gmail.com>
2020-02-16 01:27:02 +0900

    Calc viewport height

--- packages/day-breeze-web/src/main.tsx
+++ packages/day-breeze-web/src/main.tsx
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom'
 import { App } from './App'
 import './main.css'
 
+function setVH() {
+  const vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+
+window.addEventListener('resize', setVH)
+window.addEventListener('DOMContentLoaded', setVH)
+
 ReactDOM.render(<App />, document.getElementById('root'))
